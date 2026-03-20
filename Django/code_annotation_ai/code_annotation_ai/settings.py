b8d36bf601009efb47739ab1a28c43be46160c02
TooHighDude420 <toohighdudepruducts@gmail.com>
2026-03-20 10:13:41 +0100

    set debug to false since site is live

--- Django/code_annotation_ai/code_annotation_ai/settings.py
+++ Django/code_annotation_ai/code_annotation_ai/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pz(+e%a)%6ov&)xiyfpo6ob6q3s*nmpjzd_j$0c@pi_^k)&ayk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["annotationai-production.up.railway.app"]
 
